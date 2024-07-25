4c6d0d1a56f4311ba345d937b85ed667799b38dd
alexej7c0 <alexej7c0@mail.ru>
2024-07-25 21:18:03 +0400

    added code

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-#🌲🌊🚁🟩🔥🏥💛🪣🏦🌥⚡️🏆
+#🌲🌊🚁🟩🔥🏥💛🧺🏦🌥⚡️🏆
+
+from map import Map
+import time
+import os
+from helicopter import Helicopter as Helico
+from pynput import keyboard
+
+
+TICK_SLEEP = 0.05
+TREE_UPDATE = 50
+FIRE_UPDATE = 100
+MAP_W, MAP_H = 20, 10
+
+field = Map(MAP_W, MAP_H)
+
+
+helico = Helico(MAP_W, MAP_H)
+
+
+MOVES ={'w': (-1, 0), 'd': (0, 1), 's': (1, 0), 'a': (0, -1)}
+def process_key(key):
+    global helico
+    c = key.char
+    if c in MOVES.keys():
+        dx, dy = MOVES[c][0], MOVES[c][1]
+        helico.move(dx, dy)
+
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=process_key)
+listener.start()
+
+
+tick = 1
+
+while True:
+    os.system("cls")
+    print("TICK", tick)
+    field.process_helicopter(helico)
+    helico.print_stats()
+    field.print_map(helico)
+    tick += 1
+    time.sleep(TICK_SLEEP)
+    if (tick % TREE_UPDATE == 0):
+        field.generate_tree()
+    if (tick % FIRE_UPDATE == 0):
+        field.update_fires()
--- map.py
+++ map.py
@@ -8,10 +8,41 @@ from utils import randcell2
 # 2 - река
 # 3 - госпиталь
 # 4 - апгрейд-шоп
+# 5 - огонь
 
-CELL_TYPES = '🟩🌲🌊🏥🏦'
+CELL_TYPES = '🟩🌲🌊🏥🏦🔥'
+TREE_BONUS = 100
+UPGRADE_COST = 5000
 
 class Map:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.cells = [[0 for i in range(w)] for j in range(h)]
+        self.generate_forest(3, 10)
+        self.generate_river(10)
+        self.generate_river(10)
+        self.generate_upgrade_shop()
+
+    def check_bounds(self, x, y):
+        if (x < 0 or y < 0 or x >= self.h or y >= self.w):
+             return False
+        return True
+    
+    def print_map(self, helico):
+        print('⬛️' * (self.w + 2))
+        for ri in range(self.h):
+            print('⬛️', end = '')
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if (helico.x == ri and helico.y == ci):
+                    print('🚁', end = '')
+                elif (cell >= 0 and cell < len(CELL_TYPES)):
+                    print(CELL_TYPES[cell] ,end = '')
+            print('⬛️')
+        print('⬛️' * (self.w + 2))
+
+
     def generate_river(self, l):
         rc = randcell(self.w, self.h)
         rx, ry = rc[0], rc[1]
@@ -23,35 +54,45 @@ class Map:
                 self.cells[rx2][ry2] = 2
                 rx, ry = rx2, ry2
                 l -= 1
-
     def generate_forest(self, r, mxr):
         for ri in range(self.h):
             for ci in range(self.w):
                 if randbool(r, mxr):
                     self.cells[ri][ci] = 1
+    def generate_tree(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0],c[1]
+        if(self.check_bounds(cx, cy) and self.cells[cx][cy] == 0):
+            self.cells[cx][cy] = 1
+    def generate_upgrade_shop(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        self.cells[cx][cy] =  4
 
 
-    def print_map(self):
-        print('⬛️' * (self.w + 2))
-        for row in self.cells:
-            print('⬛️', end = '')
-            for cell in row:
-                if (cell >= 0 and cell < len(CELL_TYPES)):
-                    print(CELL_TYPES[cell] ,end = '')
-            print('⬛️')
-        print('⬛️' * (self.w + 2))
+    def add_fire(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if self.cells[cx][cy] == 1:
+            self.cells[cx][cy] = 5
+    def update_fires(self):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if cell == 5:
+                    self.cells[ri][ci] = 0
+        for _ in range(10):
+            self.add_fire()
 
-    def check_bounds(self, x, y):
-        if (x < 0 or y < 0 or x >= self.h or y >= self.w):
-             return False
-        return True
 
-    def __init__(self, w, h):
-        self.w = w
-        self.h = h
-        self.cells = [[0 for i in range(w)] for j in range(h)]
-
-tmp = Map(20, 10)
-tmp.generate_forest(8, 10)
-tmp.generate_river(10)
-tmp.print_map()
+    def process_helicopter(self, helico):
+        c = self.cells[helico.x][helico.y]
+        if (c == 2):
+            helico.tank = helico.mxtank
+        if (c == 5 and helico.tank > 0):
+            helico.tank -= 1
+            helico.score += TREE_BONUS
+            self.cells[helico.x][helico.y] = 1
+        if (c == 4 and helico.score >= UPGRADE_COST):
+            helico.mxtank += 1
+            helico.score -= UPGRADE_COST
